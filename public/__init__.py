8a67e3dbb86722757bf16ff386f8433490bda308
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2023-11-22 19:35:11 +0100

    Database Created

--- public/__init__.py
+++ public/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "helloworld"
+    app.config['SECRET_KEY'] = '########'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, DB_NAME)
+    db.init_app(app)
 
     from .home import views
     from .log import auth
@@ -16,4 +21,18 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(posts, url_prefix="/")
 
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
