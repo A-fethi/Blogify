57e23d5cfb16904f6dba946dff6373b60e7cabb7
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2023-11-23 13:55:50 +0100

    Flask Login Setup

--- public/__init__.py
+++ public/__init__.py
@@ -3,13 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 import os
+import secrets
+import hashlib
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+secret_key = secrets.token_hex(16)
+secret_key_hashed = hashlib.sha256(secret_key.encode()).hexdigest()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '########'
+    app.config['SECRET_KEY'] = secret_key_hashed
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, DB_NAME)
     db.init_app(app)
 
@@ -21,6 +26,8 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(posts, url_prefix="/")
 
+    from .models import User
+
     create_database(app)
 
     login_manager = LoginManager()
@@ -28,11 +35,12 @@ def create_app():
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
+    def load_user(user_id):
+        return User.query.get(user_id)
+    
     return app
 
 def create_database(app):
     with app.app_context():
-        db.create_all()
+        if not path.exists(app.config['SQLALCHEMY_DATABASE_URI']):
+            db.create_all()
--- public/log.py
+++ public/log.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 auth = Blueprint("auth", __name__)
@@ -6,8 +6,14 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
     return render_template("login.html")
 
 @auth.route("/signup", methods=["GET", "POST"])
 def sign_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
     return render_template("signup.html")
